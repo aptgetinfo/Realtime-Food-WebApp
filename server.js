a7995166203f1cda2d47dfc1a34813a0d4c56e51
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-07-06 13:24:50 +0530

    five

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ const expressLayouts= require('express-ejs-layouts')
 const app = express()
 
 
+app.use(expressLayouts)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs')
+
+
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
 
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
 
-app.use(expressLayouts)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine','ejs')
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+
 
 
 
